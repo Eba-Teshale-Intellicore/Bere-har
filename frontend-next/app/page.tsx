69d76b178ac4ae248c6402c3f625973a96ce277c
Eba Teshale <ebateshale40@gmail.com>
2026-07-05 11:55:49 +0300

    some fix

--- frontend-next/app/page.tsx
+++ frontend-next/app/page.tsx
@@ -1,9 +1,11 @@
 import HomePage from "@/components/Homepage.tsx/HomePage";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <div>
       <main>
+        <Header />
         <HomePage />
       </main>
     </div>
