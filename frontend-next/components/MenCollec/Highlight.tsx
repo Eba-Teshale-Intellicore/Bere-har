0455454aad75d09b6c11a6429b305542692fb87f
Eba Teshale <ebateshale40@gmail.com>
2026-07-09 19:54:36 +0300

    ersjhd

--- frontend-next/components/MenCollec/Highlight.tsx
+++ frontend-next/components/MenCollec/Highlight.tsx
@@ -1,34 +1,44 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/src/scss/highlight.module.scss";
 import Paragraph from "@/components/Paragraph";
 import Image from "next/image";
-import img1 from "@/public/flow1.jpeg";
-import img2 from "@/public/flow2.jpeg";
 import { motion } from "framer-motion";
 import Heading from "@/components/Heading";
-
-const womenCollections = [
-  {
-    title: "Shoes",
-    image: img1,
-    area: "fir",
-  },
-  {
-    title: "Dress",
-    image: img2,
-    area: "sec",
-  },
-  {
-    title: "Jewelry",
-    image: img2,
-    area: "thir",
-  },
-];
+import { getCategories } from "@/src/api/category";
+import { getProducts } from "@/src/api/product";
 
 export default function Highlight() {
-  // const [active, setActive] = useState("all");
   const [active, setActive] = useState("all");
+  const [categories, setCategories] = useState<any[]>([]);
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [categoriesData, productsData] = await Promise.all([
+          getCategories(),
+          getProducts(),
+        ]);
+
+        setCategories(categoriesData);
+        setProducts(productsData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredProducts =
+    active === "all"
+      ? products.filter((p) => p.gendercollection?.title === "mencollection")
+      : products.filter(
+          (p) =>
+            p.gendercollection?.title === "womencollection" &&
+            p.category.category_slug === active,
+        );
 
   return (
     <>
@@ -37,7 +47,7 @@ export default function Highlight() {
           <div className={styles.herofea}>
             <div className={styles.content}>
               <p>
-                <Heading text="Men_Collection" />
+                <Heading text="Women_Collection" />
               </p>
               <ul>
                 <li
@@ -46,128 +56,78 @@ export default function Highlight() {
                 >
                   All
                 </li>
-
-                <li
-                  onClick={() => setActive("shoes")}
-                  className={active === "shoes" ? styles.active : ""}
-                >
-                  Shoes
-                </li>
-                <li
-                  onClick={() => setActive("jew")}
-                  className={active === "jew" ? styles.active : ""}
-                >
-                  Jewelery
-                </li>
-
-                <li
-                  onClick={() => setActive("bags")}
-                  className={active === "bags" ? styles.active : ""}
-                >
-                  Bags
-                </li>
+                {categories.map((e: any) => (
+                  <li
+                    key={e.id}
+                    onClick={() => setActive(e.category_slug)}
+                    className={active === e.category_slug ? styles.active : ""}
+                  >
+                    {e.category_name}
+                  </li>
+                ))}
               </ul>
             </div>
-            {active == "all" && (
-              <div className={styles.collections}>
-                {womenCollections.map((item) => (
-                  <div
-                    key={item.title}
-                    className={styles.card}
-                    style={{ gridArea: item.area }}
+            {filteredProducts.map((product) => (
+              <div key={product.id} className={styles.collections}>
+                {/* LARGE */}
+                <div className={styles.card} style={{ gridArea: "fir" }}>
+                  <motion.div
+                    className={styles.imageWrapper}
+                    whileHover={{ scale: 1.05 }}
                   >
-                    <motion.div
-                      className={styles.imageWrapper}
-                      whileHover={{ scale: 1.05 }}
-                    >
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        fill
-                        className={styles.mainImage}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </motion.div>
-                    <Paragraph text={item.title} />
-                  </div>
-                ))}
-              </div>
-            )}
-            {(active == "all" || active == "shoes") && (
-              <div className={styles.collections}>
-                {womenCollections.map((item) => (
-                  <div
-                    key={item.title}
-                    className={styles.card}
-                    style={{ gridArea: item.area }}
+                    <Image
+                      src={
+                        product.campaign?.banner ||
+                        // product.images?.[0]?.image ||
+                        "/placeholder.jpg"
+                      }
+                      alt={product.p_title}
+                      fill
+                      className={styles.mainImage}
+                      style={{ objectFit: "cover" }}
+                    />
+                  </motion.div>
+
+                  <Paragraph text={product.p_title} />
+                </div>
+
+                {/* SMALL 1 */}
+                <div className={styles.card} style={{ gridArea: "sec" }}>
+                  <motion.div
+                    className={styles.imageWrapper}
+                    whileHover={{ scale: 1.05 }}
                   >
-                    <motion.div
-                      className={styles.imageWrapper}
-                      whileHover={{ scale: 1.05 }}
-                    >
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        fill
-                        className={`${styles.mainImage}`}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </motion.div>
-                    <Paragraph text={item.title} />
-                  </div>
-                ))}
-              </div>
-            )}
-            {(active == "all" || active == "jew") && (
-              <div className={styles.collections}>
-                {womenCollections.map((item) => (
-                  <div
-                    key={item.title}
-                    className={styles.card}
-                    style={{ gridArea: item.area }}
+                    <Image
+                      src={product.images?.[0]?.image}
+                      alt={product.p_title}
+                      fill
+                      className={styles.mainImage}
+                      style={{ objectFit: "cover" }}
+                    />
+                  </motion.div>
+
+                  <Paragraph text={product.p_title} />
+                </div>
+
+                {/* SMALL 2 */}
+                <div className={styles.card} style={{ gridArea: "thir" }}>
+                  <motion.div
+                    className={styles.imageWrapper}
+                    whileHover={{ scale: 1.05 }}
                   >
-                    <motion.div
-                      className={styles.imageWrapper}
-                      whileHover={{ scale: 1.05 }}
-                    >
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        fill
-                        className={styles.mainImage}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </motion.div>
-                    <Paragraph text={item.title} />
-                  </div>
-                ))}
+                    <Image
+                      src={product.images?.[1]?.image}
+                      alt={product.p_title}
+                      fill
+                      className={styles.mainImage}
+                      style={{ objectFit: "cover" }}
+                    />
+                  </motion.div>
+
+                  <Paragraph text={product.p_title} />
+                </div>
               </div>
-            )}
-            {(active == "all" || active == "bags") && (
-              <div className={styles.collections}>
-                {womenCollections.map((item) => (
-                  <div
-                    key={item.title}
-                    className={styles.card}
-                    style={{ gridArea: item.area }}
-                  >
-                    <motion.div
-                      className={styles.imageWrapper}
-                      whileHover={{ scale: 1.05 }}
-                    >
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        fill
-                        className={styles.mainImage}
-                        style={{ objectFit: "cover" }}
-                      />
-                    </motion.div>
-                    <Paragraph text={item.title} />
-                  </div>
-                ))}
-              </div>
-            )}
+            ))}
           </div>
         </div>
       </div>
