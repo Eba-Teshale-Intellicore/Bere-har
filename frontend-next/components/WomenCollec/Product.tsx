509b8da3034d02e8042de2bcda66dd90b24c219d
Eba Teshale <ebateshale40@gmail.com>
2026-07-11 19:42:42 +0300

    lfkdbhj

--- frontend-next/components/WomenCollec/Product.tsx
+++ frontend-next/components/WomenCollec/Product.tsx
@@ -115,6 +115,8 @@ export default function Product() {
                   ))}
                 </React.Fragment>
               ))}
+            </div>
+            <div>
               <Button
                 text={
                   active === "all"
