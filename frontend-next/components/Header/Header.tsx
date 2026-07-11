1828e292f425a2e8034fe2fdcf298a154406e61d
Eba Teshale <ebateshale40@gmail.com>
2026-07-11 15:44:51 +0300

    fkjg

--- frontend-next/components/Header/Header.tsx
+++ frontend-next/components/Header/Header.tsx
@@ -118,7 +118,7 @@ export default function Header() {
                 <li>
                   <div
                     className={styles.menuItem}
-                    onClick={() => setOpenGift((prev) => !prev)}
+                    onTouchMove={() => setOpenGift((prev) => !prev)}
                   >
                     Gifts & Personalization{" "}
                     {openGift ? (
