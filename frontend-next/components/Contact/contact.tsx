dd4adadaefce2cf93f32d3c93af71e2e1341592b
Eba Teshale <ebateshale40@gmail.com>
2026-07-12 11:07:45 +0300

    dewrkj

--- frontend-next/components/Contact/contact.tsx
+++ frontend-next/components/Contact/contact.tsx
@@ -51,146 +51,106 @@ export default function Contact(props: ContactProps) {
   }, []);
   return (
     <>
-      {/* <div className={styles.sticky}>
-        <div className={styles.container}>
-          <div
-            className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}
-          >
-            <div className={`${styles.menu} ${scrolled ? styles.menus : ""}`}>
-              <Menu size={28} strokeWidth={1.5} onClick={() => setOpen(true)} />
-              <Search size={28} />
-            </div>
-            <div className={styles.logo}>
-              <div>
-                <Link href="/">
-                  <Image src={logo} alt="logo" width={80} height={60} />
-                </Link>
-              </div>
-            </div>
-            <div className={styles.contact}>
-              {isLoggedIn ? (
-                <div
-                  className={`${styles.menu} ${scrolled ? styles.menus : ""}`}
-                >
-                  <Link href="/account/register">
-                    <Button text="Contact Us" />
-                  </Link>
-                  <CircleUserRound size={24} />
-                </div>
-              ) : (
-                <Link href="/account/register">
-                  <Button text="Register" />
-                </Link>
-              )}
-            </div>
-          </div>
-        </div>
-      </div> */}
       {/* Sidebar */}
       <AnimatePresence>
-        {open && (
-          <>
-            <div className={styles.overlay} onClick={() => setOpen(false)} />
+        <>
+          <div className={styles.overlay} onClick={() => setOpen(false)} />
 
-            <motion.div
-              className={styles.sidebar}
-              initial={{ x: "-100%" }}
-              animate={{ x: 0 }}
-              exit={{ x: "-100%" }}
-              transition={{ duration: 0.3 }}
-            >
-              <div className={styles.sidebarHeader}>
-                <X size={28} onClick={() => setOpen(false)} />
-              </div>
+          <motion.div
+            className={styles.sidebar}
+            initial={{ x: "-100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "-100%" }}
+            transition={{ duration: 0.3 }}
+          >
+            <div className={styles.sidebarHeader}>
+              <X size={28} onClick={() => setOpen(false)} />
+            </div>
 
-              <ul className={styles.nav}>
-                <li>
-                  <Link
-                    href="/women-collections"
-                    onClick={() => setOpen(false)}
-                  >
-                    Women
-                  </Link>
-                </li>
+            <ul className={styles.nav}>
+              <li>
+                <Link href="/women-collections" onClick={() => setOpen(false)}>
+                  Women
+                </Link>
+              </li>
 
-                <li>
-                  <Link href="/men-collections" onClick={() => setOpen(false)}>
-                    Men
-                  </Link>
-                </li>
+              <li>
+                <Link href="/men-collections" onClick={() => setOpen(false)}>
+                  Men
+                </Link>
+              </li>
 
-                <li
-                  onMouseEnter={() => setOpenGift(true)}
-                  onMouseLeave={() => setOpenGift(false)}
-                >
-                  <div className={styles.menuItem}>
-                    Gifts & Personalization{" "}
-                    {openGift ? (
-                      <ChevronDown size={18} />
-                    ) : (
-                      <ChevronRight size={18} />
-                    )}
-                  </div>
+              <li
+                onMouseEnter={() => setOpenGift(true)}
+                onMouseLeave={() => setOpenGift(false)}
+              >
+                <div className={styles.menuItem}>
+                  Gifts & Personalization{" "}
+                  {openGift ? (
+                    <ChevronDown size={18} />
+                  ) : (
+                    <ChevronRight size={18} />
+                  )}
+                </div>
 
-                  <AnimatePresence>
-                    {openGift && (
-                      <motion.ul
-                        className={styles.subMenu}
-                        initial={{ height: 0, opacity: 0 }}
-                        animate={{ height: "auto", opacity: 1 }}
-                        exit={{ height: 0, opacity: 0 }}
-                        transition={{ duration: 0.2 }}
-                      >
-                        <li>For her</li>
-                        <li>For him</li>
-                        <li>For kids</li>
-                      </motion.ul>
-                    )}
-                  </AnimatePresence>
-                </li>
+                <AnimatePresence>
+                  {openGift && (
+                    <motion.ul
+                      className={styles.subMenu}
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: "auto", opacity: 1 }}
+                      exit={{ height: 0, opacity: 0 }}
+                      transition={{ duration: 0.2 }}
+                    >
+                      <li>For her</li>
+                      <li>For him</li>
+                      <li>For kids</li>
+                    </motion.ul>
+                  )}
+                </AnimatePresence>
+              </li>
 
-                <li onClick={() => setOpen(false)}>New Arrival</li>
-                <li onClick={() => setOpen(false)}>Jewelry</li>
-              </ul>
+              <li onClick={() => setOpen(false)}>New Arrival</li>
+              <li onClick={() => setOpen(false)}>Jewelry</li>
+            </ul>
 
-              <ul className={styles.nav}>
+            <ul className={styles.nav}>
+              <li className={styles.logout}>
+                <MapPin size={14} />
+                <Link href="/" onClick={() => setOpen(false)}>
+                  {" "}
+                  Find A Store
+                </Link>
+              </li>
+              <li>{/* <h1>{t.header.about}</h1> */}</li>
+              {isLoggedIn ? (
                 <li className={styles.logout}>
-                  <MapPin size={14} />
+                  <Contact2 size={14} />
                   <Link href="/" onClick={() => setOpen(false)}>
-                    {" "}
-                    Find A Store
+                    Contact Us
                   </Link>
                 </li>
-                <li>{/* <h1>{t.header.about}</h1> */}</li>
-                {isLoggedIn ? (
-                  <li className={styles.logout}>
-                    <Contact2 size={14} />
-                    <Link href="/" onClick={() => setOpen(false)}>
-                      Contact Us
-                    </Link>
-                  </li>
-                ) : (
-                  ""
-                )}
-                <li className={styles.langItem}>
-                  <Globe size={14} />
-                  <span>
-                    Rest of the World | English{/* <LanguageSwitcher /> */}
-                  </span>
+              ) : (
+                ""
+              )}
+              <li className={styles.langItem}>
+                <Globe size={14} />
+                <span>
+                  Rest of the World | English{/* <LanguageSwitcher /> */}
+                </span>
+              </li>
+              <li></li>
+              {isLoggedIn ? (
+                <li className={styles.logout} onClick={() => setOpen(false)}>
+                  <Link href="/">Logout</Link>
+                  <LogOut size={14} />
                 </li>
-                <li></li>
-                {isLoggedIn ? (
-                  <li className={styles.logout} onClick={() => setOpen(false)}>
-                    <Link href="/">Logout</Link>
-                    <LogOut size={14} />
-                  </li>
-                ) : (
-                  ""
-                )}
-              </ul>
-            </motion.div>
-          </>
-        )}
+              ) : (
+                ""
+              )}
+            </ul>
+          </motion.div>
+        </>
       </AnimatePresence>
     </>
   );
