be751993c66b5519760f4ae420f310d07faf8067
Eba Teshale <ebateshale40@gmail.com>
2026-07-13 14:41:08 +0300

    defergw

---
+++ frontend-next/app/wishlist/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Wishlist from "@/components/Wishlist/Wishlist";
+
+export default function Womenpage() {
+  return (
+    <>
+      <Wishlist />
+    </>
+  );
+}
---
+++ frontend-next/components/Wishlist/Wishlist.tsx
@@ -0,0 +1,175 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "@/src/scss/product.module.scss";
+import Image from "next/image";
+import img1 from "@/public/flow1.jpeg";
+import img2 from "@/public/flow2.jpeg";
+import { motion } from "framer-motion";
+import Heading from "@/components/Heading";
+import { getCategories } from "@/src/api/category";
+import { getProducts } from "@/src/api/product";
+import Button from "../Button";
+import { useWishlistContext } from "@/app/WishlistContext";
+import { Heart } from "lucide-react";
+
+const womenCollections = [
+  {
+    title: "Women's Shoes",
+    image: img2,
+  },
+  {
+    title: "Women's Ready-to-Wear",
+    image: img1,
+  },
+  {
+    title: "Women's Jewelry",
+    image: img1,
+  },
+  {
+    title: "Women's Jewelry",
+    image: img1,
+  },
+  {
+    title: "Women's Jewelry",
+    image: img1,
+  },
+];
+
+export default function Wishlist() {
+  // const [active, setActive] = useState("all");
+  const [active, setActive] = useState("all");
+  const [categories, setCategories] = useState<any[]>([]);
+  const [products, setProducts] = useState<any[]>([]);
+  const [visibleCount, setVisibleCount] = useState(5);
+  const { toggleWishlist, isWishlisted } = useWishlistContext();
+
+  useEffect(() => {
+    setVisibleCount(5);
+  }, [active]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [categoriesData, productsData] = await Promise.all([
+          getCategories(),
+          getProducts(),
+        ]);
+
+        setCategories(categoriesData);
+        setProducts(productsData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const filteredProducts =
+    active === "all"
+      ? products.filter((p) => p.gendercollection?.title === "womencollection")
+      : products.filter(
+          (p) =>
+            p.gendercollection?.title === "womencollection" &&
+            p.category.category_slug === active,
+        );
+  return (
+    <>
+      <div className={styles.sticky}>
+        <div className={styles.container}>
+          <div className={styles.herofea}>
+            <div className={styles.content}>
+              <p>
+                <Heading text="Women Collection" />
+              </p>
+              <ul>
+                <li
+                  onClick={() => setActive("all")}
+                  className={active === "all" ? styles.active : ""}
+                >
+                  All
+                </li>
+                {categories.map((e: any) => (
+                  <li
+                    key={e.id}
+                    onClick={() => setActive(e.category_slug)}
+                    className={active === e.category_slug ? styles.active : ""}
+                  >
+                    {e.category_name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <div className={styles.collections}>
+                {filteredProducts.map((product) => (
+                  <React.Fragment key={product.id}>
+                    {product.images.slice(0, visibleCount).map((img: any) => (
+                      <div key={img.id} className={styles.card}>
+                        <motion.div
+                          className={styles.imageWrapper}
+                          whileHover={{ scale: 1.05 }}
+                        >
+                          <Image
+                            src={img.image}
+                            alt={product.p_title}
+                            fill
+                            className={styles.mainImage}
+                            style={{ objectFit: "cover" }}
+                          />
+                        </motion.div>
+
+                        <div className={styles.cardInfo}>
+                          <div className={styles.tf}>
+                            <h4>{product.p_title}</h4>
+                            <button
+                              onClick={() => toggleWishlist(product.id)}
+                              className={styles.favorites}
+                            >
+                              {/* {isWishlisted(product.id) ? (
+                                <Heart fill="red" />
+                              ) : (
+                                <Heart />
+                              )} */}
+                              <Heart
+                                fill={isWishlisted(product.id) ? "red" : "none"}
+                                color={
+                                  isWishlisted(product.id)
+                                    ? "red"
+                                    : "currentColor"
+                                }
+                              />
+                            </button>
+                          </div>
+
+                          {product.variants?.length ? (
+                            <>
+                              <p>Size: {product.variants[0].size}</p>
+                              <p>Price: ${product.variants[0].price}</p>
+                            </>
+                          ) : (
+                            <p>Price: ${product.p_price}</p>
+                          )}
+                        </div>
+                      </div>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </div>
+              <div className={styles.btn}>
+                <Button
+                  text={
+                    active === "all"
+                      ? "View More"
+                      : `More ${filteredProducts[0]?.category?.category_name ?? "Category"}`
+                  }
+                  onClick={() => setVisibleCount((prev) => prev + 5)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
