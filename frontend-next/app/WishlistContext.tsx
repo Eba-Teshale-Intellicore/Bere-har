378c802ab25be5e232dcc5e0b3895ae68d846196
Eba Teshale <ebateshale40@gmail.com>
2026-07-16 11:01:54 +0300

    sere

--- frontend-next/app/WishlistContext.tsx
+++ frontend-next/app/WishlistContext.tsx
@@ -1,133 +1,133 @@
-// "use client";
+"use client";
 
-// import {
-//   createContext,
-//   useContext,
-//   useEffect,
-//   useState,
-//   ReactNode,
-// } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
 
-// import { addWishlist, deleteWishlist, getWishlist } from "@/src/api/wishlist";
+import { addWishlist, deleteWishlist, getWishlist } from "@/src/api/wishlist";
 
-// const WishlistContext = createContext<any>(null);
+const WishlistContext = createContext<any>(null);
 
-// export const useWishlistContext = () => useContext(WishlistContext);
+export const useWishlistContext = () => useContext(WishlistContext);
 
-// export default function WishlistProvider({
-//   children,
-// }: {
-//   children: ReactNode;
-// }) {
-//   const [favorites, setFavorites] = useState<any[]>([]);
-//   const [loading, setLoading] = useState(true);
+export default function WishlistProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [favorites, setFavorites] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
-//   const fetchWishlist = async () => {
-//     setLoading(true);
+  const fetchWishlist = async () => {
+    setLoading(true);
 
-//     try {
-//       const token = localStorage.getItem("accessToken");
+    try {
+      const token = localStorage.getItem("accessToken");
 
-//       if (!token) {
-//         setFavorites([]);
-//         return;
-//       }
+      if (!token) {
+        setFavorites([]);
+        return;
+      }
 
-//       const data = await getWishlist();
+      const data = await getWishlist();
 
-//       setFavorites(data);
-//     } catch (error) {
-//       console.log(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
+      setFavorites(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-//   useEffect(() => {
-//     fetchWishlist();
-//   }, []);
+  useEffect(() => {
+    fetchWishlist();
+  }, []);
 
-//   // ADD IMAGE TO WISHLIST
-//   // const addToWishlist = async (imageId: number) => {
-//   //   if (isWishlisted(imageId)) return;
+  // ADD IMAGE TO WISHLIST
+  // const addToWishlist = async (imageId: number) => {
+  //   if (isWishlisted(imageId)) return;
 
-//   //   try {
-//   //     const token = localStorage.getItem("accessToken");
+  //   try {
+  //     const token = localStorage.getItem("accessToken");
 
-//   //     if (!token) return;
+  //     if (!token) return;
 
-//   //     await addWishlist(imageId);
+  //     await addWishlist(imageId);
 
-//   //     await fetchWishlist();
-//   //   } catch (error) {
-//   //     console.log(error);
-//   //   }
-//   // };
-//   const addToWishlist = async (imageId: number) => {
-//     try {
-//       const response = await addWishlist(imageId);
+  //     await fetchWishlist();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  const addToWishlist = async (imageId: number) => {
+    try {
+      const response = await addWishlist(imageId);
 
-//       console.log("Added:", response);
+      console.log("Added:", response);
 
-//       await fetchWishlist();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+      await fetchWishlist();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-//   // REMOVE IMAGE FROM WISHLIST
-//   const removeFromWishlist = async (imageId: number) => {
-//     try {
-//       const item = favorites.find(
-//         (favorite: any) => favorite.image.id === imageId,
-//       );
+  // REMOVE IMAGE FROM WISHLIST
+  const removeFromWishlist = async (imageId: number) => {
+    try {
+      const item = favorites.find(
+        (favorite: any) => favorite.image.id === imageId,
+      );
 
-//       if (!item) return;
+      if (!item) return;
 
-//       await deleteWishlist(item.id);
+      await deleteWishlist(item.id);
 
-//       await fetchWishlist();
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+      await fetchWishlist();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-//   // CHECK HEART STATUS
-//   const isWishlisted = (imageId: number) => {
-//     return favorites.some((item: any) => item.image.id === imageId);
-//   };
+  // CHECK HEART STATUS
+  const isWishlisted = (imageId: number) => {
+    return favorites.some((item: any) => item.image.id === imageId);
+  };
 
-//   const toggleWishlist = async (imageId: number) => {
-//     console.log("Clicked image:", imageId);
+  const toggleWishlist = async (imageId: number) => {
+    console.log("Clicked image:", imageId);
 
-//     if (isWishlisted(imageId)) {
-//       console.log("Removing");
-//       await removeFromWishlist(imageId);
-//     } else {
-//       console.log("Adding");
-//       await addToWishlist(imageId);
-//     }
-//   };
+    if (isWishlisted(imageId)) {
+      console.log("Removing");
+      await removeFromWishlist(imageId);
+    } else {
+      console.log("Adding");
+      await addToWishlist(imageId);
+    }
+  };
 
-//   return (
-//     <WishlistContext.Provider
-//       value={{
-//         favorites,
+  return (
+    <WishlistContext.Provider
+      value={{
+        favorites,
 
-//         loading,
+        loading,
 
-//         isWishlisted,
+        isWishlisted,
 
-//         addToWishlist,
+        addToWishlist,
 
-//         removeFromWishlist,
+        removeFromWishlist,
 
-//         toggleWishlist,
+        toggleWishlist,
 
-//         fetchWishlist,
-//       }}
-//     >
-//       {children}
-//     </WishlistContext.Provider>
-//   );
-// }
+        fetchWishlist,
+      }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+}
