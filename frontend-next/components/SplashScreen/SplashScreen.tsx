38adf5feeb848a10666f706edf6fd06c92e379bd
Eba Teshale <ebateshale40@gmail.com>
2026-07-18 18:44:38 +0300

    rkj

--- frontend-next/components/SplashScreen/SplashScreen.tsx
+++ frontend-next/components/SplashScreen/SplashScreen.tsx
@@ -3,15 +3,15 @@
 import Image from "next/image";
 import logo from "@/public/bere-har-2.png";
 import styles from "@/src/scss/splash.module.scss";
-import Paragraph from "@/components/Paragraph";
 
 export default function SplashScreen() {
   return (
     <div className={styles.splash}>
       <Image src={logo} alt="Bere Har" width={180} height={180} priority />
+
       <div className={styles.loading}>
-        <Paragraph text="Loading" color="black" />
-        <span></span>
+        Loading
+        <span className={styles.dots}></span>
       </div>
     </div>
   );
