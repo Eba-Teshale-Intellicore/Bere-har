5cfa03b95f1c14efab8206eff4cf09ef10e2a1b5
Eba Teshale <ebateshale40@gmail.com>
2026-07-18 15:31:48 +0300

    drt

--- frontend-next/components/Home/Hero.tsx
+++ frontend-next/components/Home/Hero.tsx
@@ -98,7 +98,7 @@ export default function Hero() {
               <motion.div
                 key={current}
                 className={styles.imagebg}
-                initial={{ opacity: 0.5, x: 100 }}
+                initial={{ opacity: 0, x: 100 }}
                 animate={{ opacity: 1, x: 0 }}
                 exit={{ opacity: 0, x: -100 }}
                 transition={{ duration: 0.9 }}
