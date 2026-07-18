a23ff96b87e1f49c2a95503b1d62e38a8a980678
Eba Teshale <ebateshale40@gmail.com>
2026-07-18 19:05:47 +0300

    ekjhg

--- frontend-next/components/Button.tsx
+++ frontend-next/components/Button.tsx
@@ -1,4 +1,5 @@
 import styles from "@/src/scss/button.module.scss";
+import { motion } from "framer-motion";
 import React from "react";
 
 type ButtonProps = {
@@ -12,13 +13,31 @@ type ButtonProps = {
 export default function Button(props: ButtonProps) {
   return (
     <>
-      <button
+      <motion.button
         className={`${styles.button} ${styles[props.variant || "primary"]} ${props.className || ""}`}
         onClick={props.onClick}
       >
-        {props.text}
+        <motion.span
+          initial={{ opacity: 0, y: -80 }}
+          whileHover={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -80 }}
+          transition={{ duration: 0.3 }}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          {props.text}
+        </motion.span>
+        <motion.span
+          initial={{ opacity: 1, y: 0 }}
+          whileHover={{ opacity: 0, y: 80 }}
+          exit={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3 }}
+          viewport={{ once: false, amount: 0.3 }}
+        >
+          {props.text}
+        </motion.span>
+
         {props.icon && <span className={styles.icon}>{props.icon}</span>}
-      </button>
+      </motion.button>
     </>
   );
 }
