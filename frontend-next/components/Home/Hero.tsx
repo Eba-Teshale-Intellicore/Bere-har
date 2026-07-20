036a1b22ff0064a35f4ede372c825b659ba626a7
Eba Teshale <ebateshale40@gmail.com>
2026-07-20 11:29:31 +0300

    derer

--- frontend-next/components/Home/Hero.tsx
+++ frontend-next/components/Home/Hero.tsx
@@ -127,7 +127,7 @@ export default function Hero() {
                   <div className={styles.heroTitle}>
                     <Heading
                       text="HandMade Ethiopian Silk "
-                      size="xl"
+                      size="lg"
                       color="primary"
                     />
                   </div>
