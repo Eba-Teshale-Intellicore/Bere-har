a262e45da6533f1bf228f297cb527ebe86a45d0b
Eba Teshale <ebateshale40@gmail.com>
2026-07-22 16:21:47 +0300

    rktj

--- frontend-next/components/Location/Locations.tsx
+++ frontend-next/components/Location/Locations.tsx
@@ -4,33 +4,33 @@ import styles from "@/src/scss/product.module.scss";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import Heading from "@/components/Heading";
-import { getCategories } from "@/src/api/category";
-import { getProducts } from "@/src/api/product";
 import Button from "../Button";
 import { useWishlistContext } from "@/app/WishlistContext";
 import { Heart } from "lucide-react";
-import { useAuth } from "@/app/AuthProvider";
-import { useRouter } from "next/navigation";
-import ProductSkeleton from "@/components/ProductSkeleton";
+import { getWishlist } from "@/src/api/wishlist";
 
-export default function Products() {
-  const router = useRouter();
+interface WishlistItem {
+  id: number;
 
-  const [loading, setLoading] = useState(true);
+  product: {
+    id: number;
+    p_title: string;
+    variants: {
+      image: string;
+      price: number;
+      size?: {
+        name: string;
+      };
+    }[];
+  };
+}
+
+export default function Wishlist() {
   const [active, setActive] = useState("all");
-  const [categories, setCategories] = useState<any[]>([]);
-  const [products, setProducts] = useState<any[]>([]);
-  const [visibleCount, setVisibleCount] = useState(10);
-  const { toggleWishlist, isWishlisted } = useWishlistContext();
-
-  const { isLoggedIn } = useAuth();
+  const [visibleCount, setVisibleCount] = useState(5);
+  const { favorites, toggleWishlist, isWishlisted } = useWishlistContext();
 
   const handleWishlist = async (id: number) => {
-    if (!isLoggedIn) {
-      router.push("/account/login");
-      return;
-    }
-
     await toggleWishlist(id);
   };
 
@@ -38,34 +38,6 @@ export default function Products() {
     setVisibleCount(5);
   }, [active]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-
-        const [categoriesData, productsData] = await Promise.all([
-          getCategories(),
-          getProducts(),
-        ]);
-
-        setCategories(categoriesData);
-        setProducts(productsData);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const product = products.filter((p) => p.p_status === "published");
-
-  const filteredProducts =
-    active === "all"
-      ? product
-      : product.filter((p) => p.category.category_slug === active);
   return (
     <>
       <div className={styles.sticky}>
@@ -73,46 +45,43 @@ export default function Products() {
           <div className={styles.herofea2}>
             <div className={styles.content}>
               <h1>
-                <Heading text="Our_Products" />
+                <Heading text="Your_Favorites" />
               </h1>
-              <ul>
-                <li
-                  onClick={() => setActive("all")}
-                  className={active === "all" ? styles.active : ""}
-                >
-                  All
-                </li>
-                {categories.map((e: any) => (
-                  <li
-                    key={e.id}
-                    onClick={() => setActive(e.category_slug)}
-                    className={active === e.category_slug ? styles.active : ""}
-                  >
-                    {e.category_name}
-                  </li>
-                ))}
-              </ul>
             </div>
             <div>
-              <div className={styles.collections}>
-                {loading
-                  ? Array.from({ length: 10 }).map((_, index) => (
-                      <ProductSkeleton key={index} />
-                    ))
-                  : filteredProducts.slice(0, visibleCount).map((product) => {
-                      const liked = isWishlisted(product.id);
-                      return (
-                        <div key={product.id} className={styles.card}>
-                          <div className={styles.overlay} />
+              {favorites.length === 0 ? (
+                <div className={styles.emptyWishlist}>
+                  <Heart size={60} fill="red" color="red" />
 
+                  <h2>Your Wishlist is Empty</h2>
+
+                  <p>
+                    Save your favorite products to your wishlist so you can
+                    easily find them later.
+                  </p>
+
+                  <Button
+                    text="Explore Collections"
+                    onClick={() => {
+                      window.location.href = "/products";
+                    }}
+                  />
+                </div>
+              ) : (
+                <>
+                  <div className={styles.collections}>
+                    {favorites.slice(0, visibleCount).map((item: any) => {
+                      const liked = isWishlisted(item.product.id);
+                      return (
+                        <div key={item.id} className={styles.card}>
                           <motion.div
                             className={styles.imageWrapper}
-                            whileHover={{ scale: 1.04 }}
+                            whileHover={{ scale: 1.05 }}
                           >
                             <motion.button
                               type="button"
                               aria-label="Add to wishlist"
-                              onClick={() => handleWishlist(product.id)}
+                              onClick={() => handleWishlist(item.product.id)}
                             >
                               <Heart
                                 fill={liked ? "red" : "none"}
@@ -124,13 +93,10 @@ export default function Products() {
                             <div>
                               <Image
                                 src={
-                                  product.variants?.[0]?.image ||
+                                  item.product.variants?.[0]?.image ||
                                   "/placeholder.jpg"
                                 }
-                                alt={
-                                  product.variants?.[0]?.alt_text ||
-                                  product.p_title
-                                }
+                                alt={item.product.p_title}
                                 fill
                                 className={styles.mainImage}
                                 style={{ objectFit: "cover" }}
@@ -139,39 +105,43 @@ export default function Products() {
                           </motion.div>
 
                           <div className={styles.cardContent}>
-                            {product.variants?.length > 0 ? (
+                            {item.product.variants?.length > 0 ? (
                               <>
                                 <div className={styles.info}>
                                   <p className={styles.price}>
-                                    ${product.variants[0].price} USD
+                                    ${item.product.variants[0]?.price} USD
                                   </p>
-                                  <p>{product.variants[0].size.name}</p>
+                                  <p>
+                                    Size:
+                                    {item.product.variants[0]?.size?.name}
+                                  </p>
                                 </div>
                                 <div className={styles.info2}>
-                                  <h4>{product.p_title}</h4>
+                                  <h4>{item.product.p_title}</h4>
                                 </div>
                               </>
                             ) : (
-                              // <p>No variants available</p>
-                              <p></p>
+                              <p>No Wishlist Availlable</p>
                             )}
                           </div>
                         </div>
                       );
                     })}
-              </div>
-              <div className={styles.btn}>
-                {visibleCount < filteredProducts.length && (
-                  <Button
-                    text="View More"
-                    onClick={() => setVisibleCount((prev) => prev + 5)}
-                  />
-                )}
-              </div>
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
+                  </div>
+                  <div className={styles.btn}>
+                    {visibleCount < favorites.length && (
+                      <Button
+                        text="View More"
+                        onClick={() => setVisibleCount((prev) => prev + 5)}
+                      />
+                    )}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
