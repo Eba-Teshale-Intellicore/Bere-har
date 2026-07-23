30e893a5df5fb91a838da1593d341fc28606ec0c
Eba Teshale <ebateshale40@gmail.com>
2026-07-23 14:20:44 +0300

    gkjh

--- frontend-next/components/Location/Locations.tsx
+++ frontend-next/components/Location/Locations.tsx
@@ -54,11 +54,14 @@ export default function Wishlist() {
                 <div className={styles.contact}>
                   <div className={styles.map}>
                     <GoogleMap
+                      mapContainerStyle={{
+                        height: "500px",
+                      }}
                       center={{
                         lat: Number(locations[0]?.latitude ?? 9.03),
                         lng: Number(locations[0]?.longitude ?? 38.74),
                       }}
-                      zoom={10}
+                      zoom={7}
                     >
                       {locations.map((location) => (
                         <Marker
