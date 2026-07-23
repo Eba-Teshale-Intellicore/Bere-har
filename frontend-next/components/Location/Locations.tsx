de0980de518dcf49b6bdd223bdf984c6b3e88a24
Eba Teshale <ebateshale40@gmail.com>
2026-07-23 13:59:44 +0300

    fkrk

--- frontend-next/components/Location/Locations.tsx
+++ frontend-next/components/Location/Locations.tsx
@@ -55,7 +55,7 @@ export default function Wishlist() {
                   <div className={styles.map}>
                     <GoogleMap
                       mapContainerStyle={{
-                        width: "50%",
+                        width: "100%",
                         height: "500px",
                       }}
                       center={{
